53df01b566e6cb04d7b9cdb125e08566512661de
jiwon <pg15320@gmail.com>
2025-02-20 16:38:55 +0900

    properties update

--- src/main/java/com/kuad/soma/repository/MemberRepository.java
+++ src/main/java/com/kuad/soma/repository/MemberRepository.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
     @Query("SELECT m FROM Member m JOIN FETCH m.team")
     List<Member> findAllWithTeam();
 
